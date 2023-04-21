6a6a014e190dd94600ac3e3a0049c8782b4d58b9
Ganwno <130082545+Ganwno@users.noreply.github.com>
2023-04-21 07:55:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,33 @@ function App() {
     <div className="bg-zinc-900 flex justify-center items-center h-screen text-white w-screen p-10">
       <div>
         <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl pb-10 font-bold text-center">
-          SHJZ 🌈{" "}
+          SalesForce Trading site
           <a
             href="/"
             className="underline hover:text-[#356DF3]"
           >
-            RainbowKit
+            "Empowering Your Trades with Salesforce."
           </a>{" "}
           +{" "}
           <a
             href="https://vitejs.dev"
             className="underline hover:text-[#356DF3]"
           >
-            Vite
+            🔐
           </a>{" "}
           +{" "}
           <a
             href="https://reactjs.org"
             className="underline hover:text-[#356DF3]"
           >
-            React
+            Secure
           </a>{" "}
           +{" "}
           <a
             href="https://tailwindcss.com"
             className="underline hover:text-[#356DF3]"
           >
-            Tailwind
+            Safe
           </a>
           !
         </h1>
